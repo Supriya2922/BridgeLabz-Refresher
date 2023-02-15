31c1f26cac9cd373da408e507ef579a7a53255d2
Supriya2922 <supriyaprakash2229@gmail.com>
2023-02-15 12:12:34 +0530

    [Supriya]-10 Programs added

---
+++ Program1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more 
+using System;
+
+namespace Program1
+{
+    internal class Program
+    {
+        public void display(String str)
+        {
+            Console.WriteLine(str);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name of my five friends:");
+            Program obj = new Program();
+            obj.display("Supritha");
+            obj.display("Keerthi");
+            obj.display("Kavya");
+            obj.display("Deepa");
+            obj.display("Usha");
+        }
+    }
+}
---
+++ Program4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Program4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number");
+        String num = Console.ReadLine();
+        Console.WriteLine($"10 * {num}= {Convert.ToDecimal(num) * 10}");
+    }
+}
