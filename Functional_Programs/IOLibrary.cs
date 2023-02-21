73d8f63d5d2b80c44217db7692ffc973fdca770b
Supriya2922 <supriyaprakash2229@gmail.com>
2023-02-21 12:15:54 +0530

    [Supriya]-Added 2D Array I/O program

---
+++ Functional_Programs/IOLibrary.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Programs
+{
+     public class IOLibrary
+    {
+        public static int[,] readInt2DArray()
+        {
+            Console.WriteLine("Enter row length");
+            
+            int r= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter column length");
+            int c= Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[r,c];
+            Console.WriteLine("Enter the numbers");
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return arr;
+        }
+        public static double[,] readDouble2DArray()
+        {
+            Console.WriteLine("Enter row length");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter column length");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double[,] arr = new double[r, c];
+            Console.WriteLine("Enter the numbers");
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    arr[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            return arr;
+        }
+        public static bool[,] readBoolean2DArray()
+        {
+            Console.WriteLine("Enter row length");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter column length");
+            int c = Convert.ToInt32(Console.ReadLine());
+            bool[,] arr = new bool[r, c];
+            Console.WriteLine("Enter the numbers");
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    arr[i, j] = Convert.ToBoolean(Console.ReadLine());
+                }
+            }
+            return arr;
+        }
+        public static int[,] printInt2DArray(int[,] arr)
+        {
+            int r =arr.GetLength(0);
+            int c = arr.GetLength(1);
+            
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return arr;
+        }
+        public static double[,] printDouble2DArray(double[,] arr)
+        {
+            int r = arr.GetLength(0);
+            int c = arr.GetLength(1);
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return arr;
+        }
+        public static bool[,] printBoolean2DArray(bool[,] arr)
+        {
+            int r = arr.GetLength(0);
+            int c = arr.GetLength(1);
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ Functional_Programs/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Functional_Programs
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(; ; )
+            {
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1.2D Array I/O Operations");
+                Console.WriteLine("2.Sum of three Integer adds to ZERO");
+                Console.WriteLine("3.Euclidean distance from the point (x, y) to the origin (0, 0).");
+                Console.WriteLine("4.Calculate Wind Chill");
+                Console.WriteLine("Enter your choice:");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch(choice) {
+                    case 1:
+                        for(; ; )
+                        {
+                            Console.WriteLine("Choose the type of 2D array to read and print:");
+                            Console.WriteLine("1.Int");
+                            Console.WriteLine("2.Boolean");
+                            Console.WriteLine("3.Double");
+                            Console.WriteLine("4.Exit");
+                            Console.WriteLine("Enter your choice:");
+                            int ch=Convert.ToInt32(Console.ReadLine());
+                            if (ch == 1)
+                            {
+                              
+                                int[,] arr = IOLibrary.readInt2DArray();
+                                IOLibrary.printInt2DArray(arr);
+                            }
+                            else if (ch == 2)
+                            {
+                                bool[,] arr = IOLibrary.readBoolean2DArray();
+                                IOLibrary.printBoolean2DArray(arr);
+                            }
+                            else if (ch == 3)
+                            {
+                                double[,] arr = IOLibrary.readDouble2DArray();
+                                IOLibrary.printDouble2DArray(arr);
+                            }
+                            else if (ch == 4)
+                                break;
+                        }
+                        break;
+                }
+
+            }
+           
+        }
+    }
+}
